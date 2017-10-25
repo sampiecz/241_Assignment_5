c6783449b759258483a98ecd48a56d3f742b0638
Samuel Piecz <z1732715@turing.cs.niu.edu>
2017-10-24 20:47:58 -0500

    Initial commit.

---
+++ Stack.h
@@ -0,0 +1,28 @@
+#ifndef STACK_H
+#define STACK_H
+    
+Class Stack
+{
+    // Data members
+    private:
+
+    // Methods
+    public:
+        Stack::Stack()
+        Stack::~Stack()
+        Stack::Stack(const Stack& other)
+        Stack& Stack::operator=(const Stack& other)
+        ostream& operator<<(ostream& lhs, const Stack& rhs)
+        size_t Stack::size() const
+        size_t Stack::capacity() const
+        bool Stack::empty() const
+        int Stack::top() const
+        void Stack::push(int val)
+        void Stack::pop()
+        void Stack::reserve(size_t n)
+        int Stack::operator[](size_t n) const
+        int& Stack::operator[](size_t n)
+        bool Stack::operator==(const Stack& rhs) const
+}; 
+
+#endif
